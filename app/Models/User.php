4ccbdbf1646ed75f2c9c43cfb1e4c6f4b6abcf50
Kick Groen <kick.groentt@gmail.com>
2026-06-01 15:37:31 +0200

    Added userPosts function

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function usersPosts() {
+        return $this->hasMany(Post::class, 'user_id');
+    }
 }
