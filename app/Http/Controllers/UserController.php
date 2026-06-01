4c6787374522cbaa0d8a83e34a06eb12942b44b3
Kick Groen <kick.groentt@gmail.com>
2026-06-01 12:25:03 +0200

    Added encrypted password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -12,6 +13,10 @@ class UserController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required', 'min:8', "max:200"]
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        User::create($incomingFields);
+
         return 'Hello from our controller';
     }
 }
