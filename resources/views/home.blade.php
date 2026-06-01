e912d239ba73b2fbaf7725ac616d6e882dd12df2
Kick Groen <kick.groentt@gmail.com>
2026-06-01 14:52:58 +0200

    Added logging in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,15 @@
             <button>Register</button>
         </form>
     </div>
+    <div style="border: 3px solid black;">
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input name='loginname' type="text" placeholder="name">
+            <input name='loginpassword' type="password" placeholder="password">
+            <button>Log in</button>
+        </form>
+    </div>
     @endauth
 </body>
 </html>
