395ae506b9f42fc3f7a7ecd350526d465f9b481d
Kick Groen <kick.groentt@gmail.com>
2026-06-02 17:09:36 +0200

    Update home.blade.php
    Moved login / register sections to their own designated pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,25 +36,7 @@
     
 
     @else
-    <div style="border: 3px solid black;">
-        <h2>Register</h2>
-        <form action="/register" method="POST">
-            @csrf
-            <input name='name' type="text" placeholder="name">
-            <input name='email' type="text" placeholder="email">
-            <input name='password' type="password" placeholder="password">
-            <button>Register</button>
-        </form>
-    </div>
-    <div style="border: 3px solid black;">
-        <h2>Login</h2>
-        <form action="/login" method="POST">
-            @csrf
-            <input name='loginname' type="text" placeholder="name">
-            <input name='loginpassword' type="password" placeholder="password">
-            <button>Log in</button>
-        </form>
-    </div>
+    <?php redirect()->route('login') ?>
     @endauth
 </body>
 </html>
