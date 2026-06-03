2666c14bfd81c538d6eb354337db66c3053ca8bb
Kick Groen <kick.groentt@gmail.com>
2026-06-03 13:12:35 +0200

    Update post.blade.php
    Added post information

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,10 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <h1>{{$post->title}} - {{$post->author->name}} ({{$post->author->email}})</h1>
+        <p>{{$post->body}}</p>
+        <p><a href="/">Go Back</a></p>
+    </div>
 </body>
 </html>
