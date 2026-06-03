d7cf70431a75138b2e3ca959c9ffc8190d10728f
Kick Groen <kick.groentt@gmail.com>
2026-06-03 13:37:35 +0200

    Update post.blade.php
    Added the section to create a post and to see a post if post given

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,25 @@
     <title>Document</title>
 </head>
 <body>
+    @isset($post)
     <div>
         <h1>{{$post->title}} - {{$post->author->name}} ({{$post->author->email}})</h1>
         <p>{{$post->body}}</p>
         <p><a href="/">Go Back</a></p>
+    <div>
+    @else
+    <div style="border: 3px solid black; padding-left: 10px;">
+        <h2>Create a New Post</h2>
+        <form action="/create-post" method="POST">
+            @csrf
+            <input name="name" type="text" value="{{$user->name}}" disabled>
+            <input name="email" type="email" value="{{$user->email}}" disabled>
+            <input type="text" name="title" placeholder="title">
+            <textarea name="body" placeholder="body content..."></textarea>
+            <button>Save Post</button>
+        </form>
     </div>
+    @endisset
+    
 </body>
 </html>
